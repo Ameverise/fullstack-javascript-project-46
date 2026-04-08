e1b36fddcfdbcd2abecce31f97db471f50884e4f
Ameverise <127594195+Ameverise@users.noreply.github.com>
2026-04-08 13:03:56 +0300

    hhhh

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,27 +1,27 @@
-import genDiff from '../src/index.js';
-import path from 'path';
-import fs from 'fs';
+import genDiff from '../src/index.js'
+import path from 'path'
+import fs from 'fs'
 
-const getFixturePath = (filename) =>
-  path.join(process.cwd(), '__fixtures__', filename);
+const getFixturePath = (filename)
+  path.join(process.cwd(), '__fixtures__', filename)
 
-const readFixture = (filename) =>
-  fs.readFileSync(getFixturePath(filename), 'utf-8');
+const readFixture = (filename)
+  fs.readFileSync(getFixturePath(filename), 'utf-8')
 
-const expected = readFixture('resultStylish.txt');
+const expected = readFixture('resultStylish.txt')
 
 test('gendiff json stylish', () => {
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
+  const file1 = getFixturePath('file1.json')
+  const file2 = getFixturePath('file2.json')
 
-  console.log(genDiff(file1, file2));
+  console.log(genDiff(file1, file2))
 
-  expect(genDiff(file1, file2)).toBe(expected);
-});
+  expect(genDiff(file1, file2)).toBe(expected)
+})
 
 test('gendiff yaml stylish', () => {
-  const file1 = getFixturePath('file1.yaml');
-  const file2 = getFixturePath('file2.yaml');
+  const file1 = getFixturePath('file1.yaml')
+  const file2 = getFixturePath('file2.yaml')
 
-  expect(genDiff(file1, file2)).toBe(expected);
-});
+  expect(genDiff(file1, file2)).toBe(expected)
+})
